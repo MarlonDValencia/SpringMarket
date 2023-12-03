ed857ec0b6dccaa62a523021515b63cfa7d85b78
Marlon Valencia <marlondvalencia@gmail.com>
2023-12-03 12:14:16 -0500

    First controller done and Spring Data JPA setted up

---
+++ src/main/java/com/java/market/web/controller/MarketController.java
@@ -0,0 +1,11 @@
+package com.java.market.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api")
+public class MarketController {
+}
